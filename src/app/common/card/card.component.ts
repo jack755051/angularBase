0671a01372dce5b22ec75ed317934c5ce660e0c7
jack755051 <jack755051@gmail.com>
2023-12-17 15:53:56 +0800

    改良card的css-01

--- src/app/common/card/card.component.ts
+++ src/app/common/card/card.component.ts
@@ -8,11 +8,16 @@ import { cardData, cardTitleType } from 'src/interface/card.interface';
 })
 export class CardComponent implements OnInit {
   @Input() cardData: cardData[] = [];
-  // @Input() cardDataTitle!: cardTitleType;
+  @Input() cardDataTitle!: cardTitleType;
 
   // cardDataTitle = cardDataTitle;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  //查看更多_進入商品頁面
+  check_more_card(i: number) {
+    console.log("序列index", i)
+  }
 }
